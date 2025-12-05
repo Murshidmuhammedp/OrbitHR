3eb5fb56afc924679c2e8c13e7d939fd36851c32
Murshidmuhammedp <murshid.mmp@gmail.com>
2025-12-06 00:10:11 +0530

    cookie

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,10 +4,9 @@ import './App.css'
 function App() {
 
   return (
-     <h1 className="text-3xl font-bold underline bg-green-800">
-    Hello world!
-  </h1>
+    <h1 className="text-3xl font-bold underline bg-green-800">
+      Hello world!
+    </h1>
   )
 }
-
 export default App
