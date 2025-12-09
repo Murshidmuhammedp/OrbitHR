f4dc016390b4a54f2c90cbf7774a682054430f97
Murshidmuhammedp <murshid.mmp@gmail.com>
2025-12-09 23:50:45 +0530

    server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,39 @@
-import app from "./app";
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+import { connectDB } from "./config/db";
+import authRoutes from "./routes/authRoutes"
+import superAdminRoutes from "./routes/superAdminRoutes"
+import { errorHandler } from "./middlewares/errorHandler";
 import 'colors';
+
+
+// import cookieParser from "cookie-parser";
+// import logger from "./middlewares/logger";
+// import corsOptions from "./config/corsOptions"; 
+
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+// app.use(cookieParser());
+// app.use(logger);
+
+
+
+app.use("/api/auth", authRoutes);
+app.use("api/superadmin", superAdminRoutes);
+
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`.bgMagenta.bold);
+    console.log(`Server running on  http://localhost:${PORT}`.bgMagenta.bold);
 });
+
+export default app;
