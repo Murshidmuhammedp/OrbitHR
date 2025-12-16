58dadf86e3cf12336e772d43c79c46fda7b15b4c
Murshidmuhammedp <murshid.mmp@gmail.com>
2025-12-17 00:03:51 +0530

    routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,9 +3,10 @@ import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 import { connectDB } from "./config/db";
-import authRoutes from "./routes/authRoutes"
-import superAdminRoutes from "./routes/superAdminRoutes"
+import authRoutes from "./routes/authRoutes";
+import superAdminRoutes from "./routes/superAdminRoutes";
 import hrRoutes from "./routes/hrRoutes";
+import employeeRoutes from "./routes/employeeRoutes";
 import { errorHandler } from "./middlewares/errorHandler";
 import 'colors';
 
@@ -26,8 +27,8 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/superadmin", superAdminRoutes);
-app.use("/api/hr",hrRoutes);
-
+app.use("/api/hr", hrRoutes);
+app.use("/api/employee", employeeRoutes);
 
 app.use(errorHandler);
 
--- server/src/routes/employeeRoutes.ts
+++ server/src/routes/employeeRoutes.ts
@@ -1,12 +1,16 @@
 import { Router } from "express";
-import { applyLeave, clockIn, clockOut, getMyProfile } from "../controllers/employeeController";
+import { applyLeave, clockIn, clockOut, getMyLeaves, getMyProfile } from "../controllers/employeeController";
 
 const router = Router();
 
-router.get("/profile",getMyProfile);
+router.get("/profile", getMyProfile);
 
-router.post("/attendance/clock-in",clockIn);
+router.post("/attendance/clock-in", clockIn);
 
-router.post("/attendance/clock-out",clockOut);
+router.post("/attendance/clock-out", clockOut);
 
-router.post("/leave",applyLeave);
+router.post("/leave", applyLeave);
+
+router.get("/leave", getMyLeaves);
+
+export default router;
